b253efbb5ed9f05ad7d5b181b420c072f6e02642
Prajwal Bhimrao Kalashetty <110298413+Hubber86@users.noreply.github.com>
2026-06-15 16:04:49 +0530

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./lib/**/*.{ts,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+        secondary: "#4f46e5",
+      },
+    },
+  },
+
+  plugins: [],
+};
+
+export default config;
