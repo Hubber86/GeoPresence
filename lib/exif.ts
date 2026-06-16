aecf061654a23505c66846ea2f11cc04b5269626
Prajwal Bhimrao Kalashetty <110298413+Hubber86@users.noreply.github.com>
2026-06-16 12:11:57 +0530

    Update exif.ts

--- lib/exif.ts
+++ lib/exif.ts
@@ -1,17 +1,65 @@
 import * as exifr from "exifr";
 
+import { format } from "date-fns";
+
+import { PhotoMetadata } from "./types";
+
 export async function extractExif(
-  filePath: string
-) {
-  const exif = await exifr.parse(filePath, {
-    gps: true
-  });
+  filePath: string,
+  fileName: string
+): Promise<PhotoMetadata> {
+  const exif = await exifr.parse(
+    filePath,
+    {
+      gps: true,
+    }
+  );
+
+  const date =
+    exif?.DateTimeOriginal ||
+    exif?.CreateDate ||
+    new Date();
 
   return {
-    latitude: exif?.latitude,
-    longitude: exif?.longitude,
-    date: exif?.DateTimeOriginal,
-    camera:
-      `${exif?.Make || ""} ${exif?.Model || ""}`
+    fileName,
+
+    timestamp:
+      new Date(date).getTime(),
+
+    dateTaken: format(
+      date,
+      "yyyy-MM-dd"
+    ),
+
+    timeTaken: format(
+      date,
+      "HH:mm:ss"
+    ),
+
+    latitude:
+      exif?.latitude,
+
+    longitude:
+      exif?.longitude,
+
+    camera: [
+      exif?.Make,
+      exif?.Model,
+    ]
+      .filter(Boolean)
+      .join(" ")
+      .trim(),
+
+    address: "",
+
+    city: "",
+
+    district: "",
+
+    state: "",
+
+    country: "",
+
+    postalCode: "",
   };
 }
