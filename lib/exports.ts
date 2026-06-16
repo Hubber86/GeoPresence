330854704cb6bbda775dc52620eeb38b89478b36
Prajwal Bhimrao Kalashetty <110298413+Hubber86@users.noreply.github.com>
2026-06-16 11:05:05 +0530

    Create exports.ts

---
+++ lib/exports.ts
@@ -0,0 +1,39 @@
+import Papa from "papaparse";
+import * as XLSX from "xlsx";
+
+export function createCsv(
+  data: unknown[]
+): string {
+  return Papa.unparse(data);
+}
+
+export function createJson(
+  data: unknown[]
+): string {
+  return JSON.stringify(
+    data,
+    null,
+    2
+  );
+}
+
+export function createExcel(
+  data: unknown[]
+): Buffer {
+  const workbook =
+    XLSX.utils.book_new();
+
+  const worksheet =
+    XLSX.utils.json_to_sheet(data);
+
+  XLSX.utils.book_append_sheet(
+    workbook,
+    worksheet,
+    "Attendance"
+  );
+
+  return XLSX.write(workbook, {
+    type: "buffer",
+    bookType: "xlsx",
+  });
+}
